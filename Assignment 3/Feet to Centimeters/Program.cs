ff3391ae25782f7d76ed728421608c2ca8ef82ab
KAMVA <lucashanisi@gmail.com>
2025-09-08 12:32:32 +0200

    Add initial project files for Feet to Centimeters app
    - Added .claudiaideconfig for background image settings.
    - Created App.config for application configuration.
    - Added Feet to Centimeters.csproj with project properties.
    - Created Feet to Centimeters.sln for Visual Studio setup.
    - Implemented main logic in Program.cs for conversion.
    - Added AssemblyInfo.cs for assembly metadata.
    - Included README.md with usage instructions and examples.

---
+++ Assignment 3/Feet to Centimeters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feet_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int feet = 5;
+
+            int inches = 7;
+
+            // convert feet to inches
+
+            int totalInches = (feet * 12) + inches;
+
+            // convert inches to centimeters
+
+            double centimeters = totalInches * 2.54;
+
+            System.Console.WriteLine("The height in centimeters is: " + centimeters);
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment 3/Feet to Centimeters/README.md
@@ -0,0 +1,16 @@
+Instructions
+Eg:
+
+feet = 5
+
+inches = 7
+
+output: 170.18 cm
+
+
+
+Formula:
+
+1 inch = 2.54 cm
+
+1 feet = 12 inches
