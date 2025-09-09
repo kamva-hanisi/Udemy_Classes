b18e1ca78aa3bae40ab45ded8f733f5f22f31d7b
KAMVA <lucashanisi@gmail.com>
2025-09-09 18:07:22 +0200

    Add initial project files for Largest of ThreeNumbers
    Created essential files including App.config, project file,
    solution file, and main program logic. Added AssemblyInfo
    for metadata and README.md for usage examples.

---
+++ Assignment 7/Largest of ThreeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_of_ThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 60;
+            int num2 = 45;
+            int num3 = 123;
+            int largest;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                largest = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                largest = num2;
+            }
+            else
+            {
+                largest = num3;
+            }
+
+            Console.WriteLine("The largest number is: " + largest);
+
+            Console.ReadLine();
+
+        }
+    }
+}
