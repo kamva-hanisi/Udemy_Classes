96b0f303ccce0418fe503adc402d7d6f96fd4b18
KAMVA <lucashanisi@gmail.com>
2025-09-09 16:57:09 +0200

    Add height categorization program and project files
    - Introduced `App.config` for configuration settings.
    - Updated `HeightCategory.csproj` to target .NET Framework 4.7.2.
    - Created/modified `HeightCategory.sln` to include the new project.
    - Implemented height categorization logic in `Program.cs`.
    - Updated `AssemblyInfo.cs` with assembly attributes.
    - Added `README.md` for usage instructions and height categories.

---
+++ Assignment 6/HeightCategory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightCategory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inches = 75;
+
+            double cm = inches * 2.54;
+
+            if (cm < 150)
+            {
+                Console.WriteLine("Dwarf");
+            }
+            else if (cm >= 150 && cm < 165)
+            {
+                Console.WriteLine("Average height");
+            }
+            else if (cm >= 165 && cm < 195)
+            {
+                Console.WriteLine("Tall");
+            }
+            else if (cm >= 165 && cm < 195)
+            {
+                Console.WriteLine("Tall");
+            }
+            else
+            {
+                Console.WriteLine("Tall");
+            }
+
+             Console.ReadLine(); 
+        }
+    }
+}
---
+++ Assignment 6/HeightCategory/README.md
@@ -0,0 +1,25 @@
+use "if" to categorize height"
+
+Height categories:
+
+Height is less than 150 cm = "Dwarf"
+
+Height is between 150 cm and 165 cm = "Average height"
+
+Height is between 165 cm and 195 cm = "Tall"
+
+Height is above 195 cm = "Abnormal height"
+
+
+
+Note: 1 inch = 2.54 centimeter
+
+You need to convert the input value (inches) into centimeters.
+
+
+
+Eg:
+
+Input: 75
+
+Output: Tall
